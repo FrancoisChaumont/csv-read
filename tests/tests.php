df597b102c1af2595a09ac50bfec8adcaa613538
Francois Chaumont <funcois@outlook.com>
2018-04-15 18:10:49 -0500

    moved sources under src folder

--- tests/tests.php
+++ tests/tests.php
@@ -1,17 +1,22 @@
 <?php
 
-require __DIR__ . "/../csv.php";
+// include the library
+require __DIR__ . "/../src/csv.php";
 
+// creates a new object and open the csv file
 $pathToFile = __DIR__ . "/tests.csv";
 $csv = new CSV($pathToFile, CSV::R);
 
+// check if the file has been opened successfully
 if (!$csv->isOpen()) { 
     die("ERROR: The file could not be opened!".PHP_EOL); 
 }
 
+// read the whole file line by line and display the content of each line
 while (($line = $csv->readLine()) != CSV::EOF) {
     var_dump($line);
 }
 
+// close the csv file
 $csv->closeFile();
 
